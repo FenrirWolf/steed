969429d757ca61dcd46a4e0d18e22d4c475dd405
Travis CI User <travis@example.org>
2017-01-15 01:37:07 +0000

    Update documentation

---
+++ implementors/steed/ops/trait.SubAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["steed"] = ["impl <a class='trait' href='steed/ops/trait.SubAssign.html' title='steed::ops::SubAssign'>SubAssign</a> for <a class='struct' href='steed/time/struct.Duration.html' title='steed::time::Duration'>Duration</a>","impl <a class='trait' href='steed/ops/trait.SubAssign.html' title='steed::ops::SubAssign'>SubAssign</a>&lt;<a class='struct' href='steed/time/struct.Duration.html' title='steed::time::Duration'>Duration</a>&gt; for <a class='struct' href='steed/time/struct.Instant.html' title='steed::time::Instant'>Instant</a>","impl <a class='trait' href='steed/ops/trait.SubAssign.html' title='steed::ops::SubAssign'>SubAssign</a>&lt;<a class='struct' href='steed/time/struct.Duration.html' title='steed::time::Duration'>Duration</a>&gt; for <a class='struct' href='steed/time/struct.SystemTime.html' title='steed::time::SystemTime'>SystemTime</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
